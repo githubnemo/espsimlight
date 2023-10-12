b925e90f0c443656659ecd778644957098d50c3f
nemo <git@ningu.net>
2023-10-12 17:43:51 +0200

    Clean up examples

--- examples/gradient.py
+++ examples/gradient.py
@@ -1,21 +1,17 @@
 def effect_fn(iterator, current_color):
-    def scale_color(c, f):
-        c.r = int(current_color.r * f)
-        c.g = int(current_color.g * f)
-        c.b = int(current_color.b * f)
-
     for i, c in enumerate(iterator):
         c.from_color(current_color)
 
-        if 3 <= i <= 5:
-            scale_color(c, 0.2)
+        if 65 <= i <= 70:
+            iterator[i] = current_color.fade_to_black(int(0.7 * 255))
         if (
-            (62 <= i <= 65)
-            or (67 <= i <= 70)
-            or (0 <= i <= 2)
-            or (6 <= i <= 8)
-            or (14 <= i <= 17)
+            (55 <= i <= 58) or
+            (61 <= i <= 67) or
+            ( 0 <= i <= 11)
         ):
-            scale_color(c, 0.4)
-        if (55 <= i <= 61) or (66 <= i <= 66) or (9 <= i <= 13) or (18 <= i <= 20):
-            scale_color(c, 0.6)
+            iterator[i] = current_color.fade_to_black(int(0.55 * 255))
+        if ((49 <= i <= 54) or
+            (59 <= i <= 60) or
+            ( 4 <= i <=  7) or
+            (12 <= i <= 14)):
+            iterator[i] = current_color.fade_to_black(int(0.4 * 255))
